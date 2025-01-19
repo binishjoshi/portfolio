27c712aadaaaeae2a03f1a58ad0516ced15ae34e
Binish <binishjoshi@pm.me>
2025-01-19 17:32:53 +0545

    chore: velite setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
+import { build } from "velite";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+export default {
+  webpack: (config) => {
+    config.plugins.push(new VeliteWebpackPlugin());
+    return config;
+  },
+} as NextConfig;
 
-export default nextConfig;
+class VeliteWebpackPlugin {
+  static started = false;
+  apply(/** @type {import('webpack').Compiler} */ compiler: any) {
+    // executed three times in nextjs
+    // twice for the server (nodejs / edge runtime) and once for the client
+    compiler.hooks.beforeCompile.tapPromise("VeliteWebpackPlugin", async () => {
+      if (VeliteWebpackPlugin.started) return;
+      VeliteWebpackPlugin.started = true;
+      const dev = compiler.options.mode === "development";
+      await build({ watch: dev, clean: !dev });
+    });
+  }
+}
