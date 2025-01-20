73831aeef1b60ee4183c4a8da085370423736dc1
Binish <binishjoshi@pm.me>
2025-01-20 10:55:07 +0545

    chore: changed next.config from ts to mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,29 @@
 import { build } from "velite";
-import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 export default {
+  // othor next config here...
   webpack: (config) => {
     config.plugins.push(new VeliteWebpackPlugin());
     return config;
   },
-} as NextConfig;
+};
 
 class VeliteWebpackPlugin {
   static started = false;
-  apply(/** @type {import('webpack').Compiler} */ compiler: any) {
-    // executed three times in nextjs
+  constructor(/** @type {import('velite').Options} */ options = {}) {
+    this.options = options;
+  }
+  apply(/** @type {import('webpack').Compiler} */ compiler) {
+    // executed three times in nextjs !!!
     // twice for the server (nodejs / edge runtime) and once for the client
     compiler.hooks.beforeCompile.tapPromise("VeliteWebpackPlugin", async () => {
       if (VeliteWebpackPlugin.started) return;
       VeliteWebpackPlugin.started = true;
       const dev = compiler.options.mode === "development";
-      await build({ watch: dev, clean: !dev });
+      this.options.watch = this.options.watch ?? dev;
+      this.options.clean = this.options.clean ?? !dev;
+      await build(this.options); // start velite
     });
   }
 }
