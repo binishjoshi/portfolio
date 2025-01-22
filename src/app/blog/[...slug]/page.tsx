708c486ef1e73ddc19a15265918f83987d4e8e1a
Binish <binishjoshi@pm.me>
2025-01-22 21:53:40 +0545

    feat: blog and homepage metadata

--- src/app/blog/[...slug]/page.tsx
+++ src/app/blog/[...slug]/page.tsx
@@ -1,13 +1,39 @@
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 import { posts } from "#site/content";
 import { MdxContent } from "@/components/mdx-components";
 
 import "@/styles/mdx.css";
+import { siteConfig } from "../../../../config/site";
 
 interface BlogViewPageProps {
   params: {
     slug: string[];
+  };
+}
+
+export async function generateMetadata({
+  params,
+}: BlogViewPageProps): Promise<Metadata> {
+  const post = await getPostFromParams(params);
+
+  if (!post) {
+    return {};
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: {
+      name: siteConfig.author,
+    },
+    openGraph: {
+      title: post.title,
+      description: post.description,
+      type: "article",
+      url: post.slug,
+    },
   };
 }
 
