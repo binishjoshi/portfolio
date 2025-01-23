f9acc420088a91ab3d3fd518541f09c2d6bf297c
Binish <binishjoshi@pm.me>
2025-01-23 11:10:10 +0545

    fix: z-index header

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -12,7 +12,7 @@ import { siteConfig } from "../../config/site";
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-10 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto flex h-14 max-w-screen-2xl items-center px-4">
         <MainNav />
         <div className="flex flex-1 items-center justify-end space-x-2">
